4f954ea76c8f4a39b365344777d7ad315806edac
Tom Close <tclose@oist.jp>
2015-06-13 15:13:52 +0900

    ran converter over check_input

--- python/check_input.py
+++ python/check_input.py
@@ -1,22 +1,22 @@
-<?php
-/* php program check_input.php */
-function check_decimal($Mvalue){
-if ( preg_match( '/^0$/', $Mvalue) ){
-	$value="1";
-	return ($value);
-} 
 
-if ( preg_match( '/^(-[1-9])[0-9]*$/', $Mvalue) ) {
-	$value="1";
-	return($value);
-}
-elseif ( preg_match( '/^([1-9])[0-9]*$/', $Mvalue) ) {
-	$value="1";
-	return($value);
-} else {
-	print "The value \"$Mvalue\" is badly formatted!<br>\n";
-	$value="0";
-	return($value);
-}
-}
-?>
+def  check_decimal(Mvalue):
+"""  php program check_input.php  """
+if  preg_match( '/^0/', Mvalue: )
+	value="1"
+	return (value)
+ 
+
+if  preg_match( '/^(-[1-9]:[0-9]/', Mvalue) ) 
+	value="1"
+	return(value)
+
+elif:
+	value="1"
+	return(value)
+ else:
+	print "The value \"Mvalue\" is badly formatted not <br>\n"
+	value="0"
+	return(value)
+
+
+
