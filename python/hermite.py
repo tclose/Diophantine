61c982edc87bc38d89b6982f60d1c302fdf5b016
Tom Close <tclose@oist.jp>
2015-06-14 14:57:27 +0900

    cleaned up hermite.py by hand

--- python/hermite.py
+++ python/hermite.py
@@ -32,16 +32,6 @@ The Havas, Majewski, Matthews LLL method is used+
 We usually take alpha=m1/n1, with (m1,n1)=(1,1) to get best results+
 """
 
-# global col1
-# global col2
-# global n + 1
-# global B
-# global L
-# global A
-# global D
-# global hnf
-# global unimodular_matrix
-# global rank
 from copy import deepcopy
 from fractions import gcd
 import numpy
@@ -52,27 +42,11 @@ def axb(Ab, m, n, m1, n1):
     A is m x n, b is m x 1, solving AX=b, X is n x 1+
     Ab is the (n+1) x m transposed augmented matrix. G=[A^t|0] [b^t]1]
     """
-    # global hnf
-    # global unimodular_matrix
-    # global rank
-#     G = deepcopy(Ab)
-    for i in xrange(m + 1):
-        for j in xrange(n):
-            G[i][j] = Ab[i][j]
+    G = deepcopy(Ab)
     for i in xrange(m):
-        G[i][n + 1] = 0
-    G[m + 1][n + 1] = 1
-#    print "G="
-#    printmat1(G,m + 1,n + 1)
-#    print "<br>\n"
-    hnf, unimodular_matrix, rank = lllhermite(G, m + 1, n + 1, m1, n1)
-#    print "HNF(G)="
-#    printmat1(hnf,m + 1,n + 1)
-#    print "<br>\n"
-#    print "P ="
-#    printmat1(unimodular_matrix,m + 1,m + 1)
-#    print "is a unimodular matrix such that PG = HNF(G)"
-#    print "<br>\n"
+        G[i, n + 1] = 0
+    G[m + 1, n + 1] = 1
+    hnf, unimodular_matrix, rank = lllhermite(G, m1, n1)
     flag = 0
     for i in xrange(rank - 1):
         if hnf[i][n + 1] != 0:
@@ -84,15 +58,11 @@ def axb(Ab, m, n, m1, n1):
             flag1 = 1
             break
     if flag == 0 and hnf[rank][n + 1] == 1 and flag1 == 0:
-        # print "<img align=\"middle\" src=\"../jpgs/matrixP.png\"><br>\n"
         y = -unimodular_matrix[rank, :]
-        # print "AX=B has a solution: Y = "
-        # print[y,m]
-        # print "<br>\n"
         nullity = m + 1 - rank
         if nullity == 0:
             print "AX=B has a unique solution in integers<br>\n"
-            return
+            return y
         else:
             lim = m + 1 - rank
             basis = []
@@ -103,51 +73,43 @@ def axb(Ab, m, n, m1, n1):
         raise Exception("AX=B has no solution in integers<br>\n")
     # joining basis and y
     basis = numpy.concatenate(basis, y)
-    shortest_distance_axb(basis, lim + 1, m)
-    return
+    return shortest_distance_axb(basis, lim + 1, m)
 
 
-def lllhermite(G, m, n, m1, n1):
+def lllhermite(G, m1, n1):
     """  G is a nonzero matrix with at least two rows.  """
     m = G.shape[0]
     n = G.shape[1]
     B = numpy.eye(m)
     L = numpy.zeros((m, m))  # Lower triangular matrix
     D = numpy.ones(m + 1)
-    for i in xrange(m):
-        for j in xrange(n):
-            A[i][j] = G[i][j]
+    A = deepcopy(G)
     flag = flagcol(A, m, n)
     if flag == 1:
-        B[m][m] = -1
-        for j in xrange(n):
-            A[m][j] = -A[m][j]
+        B[m, m] = -1
+        A[m, :] *= -1
     k = 2
     while k <= m:
-        reduce2(k, k - 1, m, n, D)
+        col1, col2 = reduce2(A, B, L, k, k - 1, m, n, D)
         minim = min(col2, n)
         temp1 = D[k - 2] * D[k]
-        temp2 = L[k][k - 1] * L[k][k - 1]
+        temp2 = L[k, k - 1] * L[k, k - 1]
         temp3 = temp1 + temp2
         u = n1 * temp3
         temp1 = D[k - 1] * D[k - 1]
         v = m1 * temp1
         if col1 <= minim or (col1 == col2 and col1 == n + 1 and u < v):
-            swap2(k, m, n)
+            swap2(A, B, L, D, k, m, n)
             if k > 2:
                 k = k - 1
         else:
             for i in xrange(k - 2 - 1, 0, -1):
                 reduce2(k, i, m, n, D)
             k = k + 1
-    for i in xrange(m):
-        for j in xrange(n):
-            hnf[i][j] = A[i][j]
-    for i in xrange(m):
-        for j in xrange(m):
-            unimodular_matrix[i][j] = B[i][j]
+    hnf = deepcopy(A)
+    unimodular_matrix = deepcopy(B)
     for i in xrange(m - 1, 0, -1):
-        test = zero_row_test(A, n, i)
+        test = zero_row_test(A, i)
         if test == 0:
             break
     rank = m - i
@@ -162,38 +124,25 @@ def lllhermite(G, m, n, m1, n1):
     return hnf, unimodular_matrix, rank
 
 
-def flagcol(A, m, n):
+def flagcol(A):
     """
     returns 0 if the first nonzero column j of A contains more than one nonzero
     entry, or contains only one nonzero entry and which is positive+ returns 1
     if the first nonzero column j of A contains only one nonzero entry, which
     is negative+ This assumes A is a nonzero matrix with at least two rows+
     """
-    flag = 0
-    for j in xrange(n):
-        for i in xrange(m):
-            # found the first column with a nonzero elt, which is in row i
-            if A[i][j] != 0:
-                flag = 1
-                break
-        if flag == 1:
-            break
-    for k in xrange(i, m):
-        if A[k][j] != 0:
-            return 0
-    if A[i][j] > 0:  # A[i][j] is the only elt in column j and is positive
-        return 0
-    else:  # A[i][j] is the only elt in column j and is negative
+    # Get the first nonzero column
+    nonzero_col = A[:, numpy.min(numpy.nonzero(numpy.sum(A, axis=0) == 0))]
+    # Get the nonzero elements
+    nonzero_elems = numpy.nonzero(nonzero_col)
+    # If there is only one and it is negative return 1 else 0
+    if len(nonzero_elems) == 1 and nonzero_elems[0] < 0:
         return 1
+    else:
+        return 0
 
 
-def reduce2(k, i, m, n, D):
-    # global col1
-    # global col2
-    # global n + 1
-    # global B
-    # global L
-    # global A
+def reduce2(A, B, L, k, i, m, n, D):
     col1 = n + 1
     for j in xrange(n):
         if A[i][j] != 0:
@@ -231,6 +180,7 @@ def reduce2(k, i, m, n, D):
         for j in xrange(i - 1):
             temp = q * L[i][j]
             L[k][j] = L[k][j] - temp
+    return col1, col2
 
 
 def minus(j, m, L):
@@ -240,12 +190,7 @@ def minus(j, m, L):
                 L[r][s] = -L[r][s]
 
 
-def swap2(k, m, n):
-    # global B
-    # global L
-    # global A
-    # global D
-    # print "Row k <. Row k - 1<br>\n"
+def swap2(A, B, L, D, k, m, n):
     for j in xrange(n):
         temp = A[k][j]
         A[k][j] = A[k - 1][j]
@@ -274,41 +219,28 @@ def swap2(k, m, n):
     return
 
 
-def zero_row_test(matrix, n, i):
+def zero_row_test(matrix, i):
     """
     This tests the i-th row of matrix to see if there is a nonzero
     entry. If there is one and the first occurs in column j, then j
     is returned. Otherwise 0 is returned
     """
-    for j in xrange(n):
-        if matrix[i][j] != 0:
-            return j
-    return 0
+    nonzero_elems = numpy.nonzero(matrix[i, :])
+    if len(nonzero_elems) == 1:
+        return nonzero_elems[0]
+    else:
+        return 0
 
 
-def shortest_distance_axb(AXB):
-    # global choleskynum
-    # global choleskyden
-    # global multnum
-    # global multden
-    # global addnum
-    # global addden
-    # global subnum
-    # global subden
-    # global rationum
-    # global ratioden
-    # global lcv
-    count = 0
-    m = AXB.shape[0]
-    n = AXB.shape[1]
+def shortest_distance_axb(A):
+    m = A.shape[0]
+    n = A.shape[1]
     m -= 1  # Not sure about this
-    AA = AXB[:-1, :]  # AA consists of the first m-1 rows of A
-    G = gram(AXB)
+    G = gram(A)
     Qn, Qd = cholesky(G)
     m -= 1
-    for i in xrange(m):  # the N vector
-        Nn[i] = Qn[i][m + 1]
-        Nd[i] = Qd[i][m + 1]
+    Nn = Qn[:, m + 1]
+    Nd = Qd[:, m + 1]
     Cn = 0
     Cd = 1
     for i in xrange(m):
@@ -316,48 +248,54 @@ def shortest_distance_axb(AXB):
         n, d = multr(n, d, Qn[i][i], Qd[i][i])
         Cn, Cd = addr(Cn, Cd, n, d)
     i = m
-    Tn[m] = Cn
-    Td[m] = Cd
-    Un[m] = 0
-    Ud[m] = 1
+    Tn = Cn
+    Td = Cd
+    Un = 0
+    Ud = 1
+    multipliers = []  # List to hold multipliers
+    xs = []  # List to hold pas values of x
     while 1:
-        Zn, Zd = ratior(Tn[i], Td[i], Qn[i][i], Qd[i][i])
-        n, d = subr(Nn[i], Nd[i], Un[i], Ud[i])
-        UB[i] = introot(Zn, Zd, n, d)
-        n, d = subr(Un[i], Ud[i], Nn[i], Nd[i])
-        x[i] = -introot(Zn, Zd, n, d) - 1
+        # Calculate UB
+        Zn, Zd = ratior(Tn, Td, Qn, Qd)
+        n, d = subr(Nn, Nd, Un, Ud)
+        UB = introot(Zn, Zd, n, d)
+        # Calculate x
+        n, d = subr(Un, Ud, Nn, Nd)
+        x = -introot(Zn, Zd, n, d) - 1
         while True:
-            x[i] += 1
-            if le(x[i], UB[i]):
+            x += 1
+            if x <= UB:
                 if i == 1:
-                    lcv = lcasvector(AA, x)
-                    count = count + 1
-                    lcva[count] = lcv
-                    coord[count] = x
-                    for k in xrange(n):
-                        temp = A[m + 1][k]
-                        mulitpliers[count][k] = temp - lcv[k]
-                    l = mulitpliers[count] ** 2  # lengthsquared(mulitpliers[count], n)
-                    mulitpliers[count][n + 1] = l
+                    lcv = lcasvector(A[:-1, :], x)
+                    multiplier = A[m + 1, :n] - lcv
+#                   lengthsquared(mulitpliers[count], n)
+                    l = multiplier ** 2
+                    multiplier[n + 1] = l
+                    multipliers.append(multiplier)
                     continue
                 else:
-                    i = i - 1
-                    # now update U[i]
-                    Un[i], Ud[i] = 0, 1
+                    # Save x in list of previous xs
+                    xs.append(x)
+                    # now update U
+                    prev_Un = Un
+                    prev_Ud = Ud
+                    Un, Ud = 0, 1
                     for j in xrange(i, m):
-                        n, d = multr(Qn[i][j], Qd[i][j], x[j], 1)
-                        Un[i], Ud[i] = addr(Un[i], Ud[i], n, d)
-                    # now update T[i]
-                    n, d = addr(x[i + 1], 1, Un[i + 1], Ud[i + 1])
-                    n, d = subr(n, d, Nn[i + 1], Nd[i + 1])
+                        # Loops from back of xs
+                        n, d = multr(Qn[j], Qd[j], xs[i - j], 1)
+                        Un, Ud = addr(Un, Ud, n, d)
+                    # now update T
+                    n, d = addr(x, 1, prev_Un, prev_Ud)
+                    n, d = subr(n, d, Nn[i], Nd[i])
                     n, d = multr(n, d, n, d)
-                    n, d = multr(Qn[i + 1][i + 1], Qd[i + 1][i + 1], n, d)
-                    Tn[i], Td[i] = subr(Tn[i + 1], Td[i + 1], n, d)
+                    n, d = multr(Qn[i][i], Qd[i][i], n, d)
+                    Tn, Td = subr(Tn, Td, n, d)
+                    i = i - 1
                     break
             else:
                 i = i + 1
                 if i > m:
-                    return mulitpliers
+                    return multipliers
                 continue
 
 
