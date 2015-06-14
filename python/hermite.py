da8da6c3d28cd33389f6c7254e1b7ebc5cba5532
Tom Close <tclose@oist.jp>
2015-06-14 11:19:52 +0900

    added cholesky into hermite.py

--- python/hermite.py
+++ python/hermite.py
@@ -695,3 +695,43 @@ def test_zeromat(A, m, n):
             if A[i][j] != 0:
                 return 0
     return 1
+
+
+def cholesky(A, m):
+    """
+    # A is positive definite mxm
+    """
+    # global rationum
+    # global ratioden
+    # global multnum
+    # global multden
+    # global subnum
+    # global subden
+    # global choleskynum
+    # global choleskyden
+    Qnum = A
+    for i in xrange(m):
+        for j in xrange(m):
+            Qden[i][j] = 1
+    for i in xrange(1, m):
+        for j in xrange(i, m):
+            Qnum[j][i] = Qnum[i][j]
+            Qden[j][i] = Qden[i][j]
+            rationum, ratioden = ratior(Qnum[i][j], Qden[i][j], Qnum[i][i],
+                                        Qden[i][i])
+            Qnum[i][j] = rationum
+            Qden[i][j] = ratioden
+    for k in xrange(i, m):
+        for l in xrange(k - 1, m):
+            multnum, multden = multr(Qnum[k][i], Qden[k][i], Qnum[i][l],
+                                     Qden[i][l])
+            t2num = multnum
+            t2den = multden
+            subnum, subden = subr(Qnum[k][l], Qden[k][l], t2num, t2den)
+            Qnum[k][l] = subnum
+            Qden[k][l] = subden
+    for i in xrange(m):
+        for j in xrange(m):
+            choleskynum[i][j] = Qnum[i][j]
+            choleskyden[i][j] = Qden[i][j]
+    return
