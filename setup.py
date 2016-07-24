a17ef5cca483a4b936f212c0dfbcb804525b44af
Tom Close <tclose@oist.jp>
2016-07-24 13:15:30 +1000

    Added setup.py, authors and requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Diophantine",
+    version="0.1",
+    package_data={'diophantine': []},
+    packages=find_packages(),
+    author="Thomas G. Close",
+    # add your name here if you contribute to the code
+    author_email="tom.g.close@gmail.com",
+    description=(
+        "A python package for finding small solutions of systems of"
+        "diophantine equations"),
+    long_description=open("README").read(),
+    license="MIT License",
+    keywords="mathematics diophantine algebra integer",
+    url="http://github.com/tclose/Diophantine",
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2',
+                 'Topic :: Scientific/Engineering'],
+    install_requires=['sympy'],
+    tests_require=['nose']
+)
