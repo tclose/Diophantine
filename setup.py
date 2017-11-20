1e6cce34398202007a30b9a4c213e7f5d799818b
Tom Close <tom.g.close@gmail.com>
2017-11-20 13:08:25 +1100

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,9 @@ setup(
         "A python package for finding small solutions of systems of "
         "diophantine (integer algebra) equations"),
     long_description=open("README.rst").read(),
-    license="MIT License",
+    license="MIT",
     keywords=["mathematics", "diophantine", "algebra", "integer",
               "systems"],
-    url="http://github.com/tclose/Diophantine/tarball/0.1",
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Console',
                  'Intended Audience :: Science/Research',
@@ -23,7 +22,13 @@ setup(
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
                  'Topic :: Scientific/Engineering'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     install_requires=['sympy'],
     tests_require=['nose']
 )
